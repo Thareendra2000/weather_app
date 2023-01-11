4e11a322c2e55f7729f6f235fe4e3dd1370295db
Thareendra2000 <thareendra2000@gmail.com>
2023-01-11 19:15:54 +0530

    edited App.js

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+  // API url
+  const url ="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=e1c10de39471b526a19ff0ad309782c9"
   return (
     <div className="App">
       
