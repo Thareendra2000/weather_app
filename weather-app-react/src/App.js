1ab34e261111dc4bda938659c211e4ac2c004da9
Thareendra2000 <thareendra2000@gmail.com>
2023-01-11 20:26:50 +0530

    change readme direction

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 
 function App() {
   // API url
-  const url ="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=e1c10de39471b526a19ff0ad309782c9"
+  //const url ="https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e1c10de39471b526a19ff0ad309782c9"
   return (
-    <div className="App">
-      
+    <div className="app">
+      .container
     </div>
   );
 }
