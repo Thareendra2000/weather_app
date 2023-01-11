d54b3b06374c2a76a8a2864f7dcb2ad303121c02
Thareendra2000 <thareendra2000@gmail.com>
2023-01-12 00:31:24 +0530

    Installed tailwindcss

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -6,7 +6,7 @@ function App() {
   //const url ="https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e1c10de39471b526a19ff0ad309782c9"
   return (
     <div className="app">
-      .container
+      <img src='Burj_khalifa.png'></img>
     </div>
   );
 }
