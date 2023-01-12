f6c99f81ffb2c65236ba1f9b90a70bdb592c07b3
Thareendra2000 <thareendra2000@gmail.com>
2023-01-12 12:12:32 +0530

    edited App.js

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,14 +1,40 @@
 import React, {useState} from 'react'
 import axios from 'axios'
+import weather from './images/w01.png'
 
 function App() {
   // API url
   //const url ="https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e1c10de39471b526a19ff0ad309782c9"
   return (
     <div className="app">
-      <h1>Hi everyone!</h1>
-      <img src='Burj_Khalifa.png'></img>
+    {/* <!-- ------search bar------ --> */}
+    <div className="search-bar">
     </div>
+    {/* <!-- --------middle section----- --> */}
+    <div className="w-data">
+        <p className="city">Dallas</p>
+        <div className="image-slider">
+        <img src={weather} /> 
+        </div>
+        <h1 className="temp">30F</h1>
+        <h2 className="tag">Clear</h2>
+    </div>
+    {/* <!-- ---------bottom part------- --> */}
+    <div className="bottom-bar">
+        <div className="data-mini">
+            <p className="data">72%</p>
+            <p className="type">Humitidy</p>
+        </div>
+        <div className="data-mini">
+            <p className="data">72%</p>
+            <p className="type">Humitidy</p>
+        </div>
+        <div className="data-mini">
+            <p className="data">72%</p>
+            <p className="type">Humitidy</p>
+        </div>
+    </div>
+   </div> 
   );
 }
 
