d699eba1157fe6979a076b0414af1484f75b2372
Thareendra2000 <thareendra2000@gmail.com>
2023-01-12 10:54:45 +0530

    removed tailwind

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -6,7 +6,8 @@ function App() {
   //const url ="https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e1c10de39471b526a19ff0ad309782c9"
   return (
     <div className="app">
-      <img src='Burj_khalifa.png'></img>
+      <h1>Hi everyone!</h1>
+      <img src='Burj_Khalifa.png'></img>
     </div>
   );
 }
